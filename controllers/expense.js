aea5f3b9fb8c87f420b58f69fbd87f315f7e0c93
Mriganka Goswami <mrigankagoswami03@gmail.com>
2021-08-06 13:41:54 +0530

    add date in add expense

--- controllers/expense.js
+++ controllers/expense.js
@@ -13,7 +13,8 @@ export const getexpenses = async (req, res) => {
 }
 export const createexpenses = async (req, res) => {
     try {
-        const { username, spent_category, spent_on, amount } = req.body;
+        const { username, spent_category, spent_on, amount, entrydate } = req.body;
+        console.log(req.body);
         const isExist = await User.findOne({ email: username });
         if (!isExist) {
             return res.status(401).json({ status: "error", body: null, msg: "User not found", error: "User not found" });
@@ -22,7 +23,8 @@ export const createexpenses = async (req, res) => {
             username,
             spent_category,
             spent_on,
-            amount
+            amount,
+            entrydate
         });
         await newdata.save();
         return res.status(201).json({ status: "ok", body: null, msg: "Expenses Added Successfully", error: null });
